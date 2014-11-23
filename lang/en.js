8118a38f2b2aacc9b028f3ee1cb1cd1498e065db
Piotrek Koszuliński <pkoszulinski@gmail.com>
2014-11-23 20:42:39 +0100

    Fixed GCC warning.

--- lang/en.js
+++ lang/en.js
@@ -1,5 +1,5 @@
 CKEDITOR.plugins.setLang( 'descriptionlist', 'en', {
 	descriptionList: 'Description List',
 	descriptionTerm: 'Description Term',
-	descriptionValue: 'Description Value',
+	descriptionValue: 'Description Value'
 } );
